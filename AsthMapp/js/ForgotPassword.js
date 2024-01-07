bf089acd749e713ad1886647b9c9bcf0200b9e0f
sh1720 <147486933+sh1720@users.noreply.github.com>
2024-01-07 05:00:57 +0000

    Add files via upload

---
+++ AsthMapp/js/ForgotPassword.js
@@ -0,0 +1,20 @@
+var buttonPrimaryContainer = document.getElementById("buttonPrimaryContainer");
+if (buttonPrimaryContainer) {
+    buttonPrimaryContainer.addEventListener("click", function (e) {
+        window.location.href = "./ForgotPassword2.html";
+    });
+}
+
+var buttonPrimary = document.getElementById("ResetPswdBtn");
+if (buttonPrimary) {
+    buttonPrimary.addEventListener("click", function (e) {
+        window.location.href = "./ForgotPassword2.html";
+    });
+}
+
+var textContainer1 = document.getElementById("textContainer1");
+if (textContainer1) {
+    textContainer1.addEventListener("click", function (e) {
+        window.location.href = "./SignUp.html";
+    });
+}
