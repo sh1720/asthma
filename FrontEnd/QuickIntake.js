8db655aeb4131235c5fb587082b044e0ded60477
sh1720 <147486933+sh1720@users.noreply.github.com>
2024-01-07 04:30:10 +0000

    Delete FrontEnd directory

--- FrontEnd/QuickIntake.js
+++
@@ -1,9 +0,0 @@
-//setting up for quick add intake
-    const cancelBtn = document.getElementById("cancelBtn");
-    let favInhalerName = document.getElementById("favInhalerName");
-
-    favInhalerName.textContent = Inhaler.favInhaler.getName();
-
-    cancelBtn.addEventListener('click', function () {
-            Inhaler.favInhaler.removeLastIntake();
-    })
